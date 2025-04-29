e637dfe676a767c6aa32f0b629533e588f8934f5
Hemanshubt <hemanshumahajan55@gmail.com>
2025-04-29 16:04:48 +0530

    first commit

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Master Degree",
+    duration: "2020 - Present",
+    institution: "R. C. Patel Institute of Management Research and Development College.",
+  },
+  // {
+  //   id: 2,
+  //   title: "Higher Secondary Certificate",
+  //   duration: "2018 - 2020",
+  //   institution: "Noakhali Islamia Kamil Madrasah",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Secondary School Certificate",
+  //   duration: "2008 - 2018",
+  //   institution: "Baitus Saif Islamia Madrasah",
+  // }
+]
---
+++ utils/data/projects-data.js
@@ -0,0 +1,52 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: 'Document OCR System',
+        description: "Developed an automated system to convert PDFs into editable Excel files using OCR with Python, Pandas, OpenCV, and Tesseract. Streamlined data extraction and formatting, boosting data entry efficiency by 40%, and improving productivity and accuracy in data management across multiple industries.",
+        tools: ['Python', 'Pandas', 'OpenCV', 'Tesseract', 'Kaggle',],
+        role: 'Machine Learning Engineer',
+        code: 'https://github.com/Pradyumna744',
+        // demo: 'https://github.com/Pradyumna744',
+    },
+    {
+        id: 2,
+        name: 'Medical Prescriptions OCR System,',
+        description: 'Developed an OCR system for medical prescriptions using TensorFlow, CRNN, and Transformer models.Achieved high accuracy in recognizing handwritten and printed medical documents.Improved healthcare workflows by reducing manual data entry errors.Utilized Docker for consistent deployment and scalability.Enhanced patient safety through accurate prescription digitization.',
+        tools: ['TensorFlow', 'CRNN', "Transformer models",],
+        role: 'Machine Learning Engineer',
+        code: 'https://github.com/Pradyumna744',
+        // demo: 'https://github.com/Pradyumna744',
+    },
+    // {
+    //     id: 3,
+    //     name: 'AI Powered Real Estate',
+    //     description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+    //     tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+    //     code: '',
+    //     role: 'Full Stack Developer',
+    //     demo: '',
+    // },
+    // {
+    //     id: 4,
+    //     name: 'Newsroom Management',
+    //     description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+    //     tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+    //     code: '',
+    //     demo: '',
+    //     role: 'Full Stack Developer',
+    // }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
